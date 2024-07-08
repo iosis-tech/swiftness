ef441387a54f9f15e7b8b6fe643daeefda818086
Bartosz Nowak <bartekn337@gmail.com>
2024-07-08 22:01:23 +0200

    fri config fix

--- crates/fri/src/tests/config.rs
+++ crates/fri/src/tests/config.rs
@@ -5,7 +5,7 @@ use starknet_crypto::Felt;
 #[test]
 fn test_fri_config() {
     let fri_config = super::config::get();
-    let log_n_cosets = Felt::from_hex("0x4").unwrap();
+    let log_n_cosets = Felt::from_hex("0x2").unwrap();
     let n_verifier_friendly_commitment_layers = Felt::from_hex("0x64").unwrap();
     let log_expected_input_degree = Felt::from_hex("0x12").unwrap();
 
