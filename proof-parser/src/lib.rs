a6e5b0514cd094901a4a044a4016bfebaf9e01b7
Pia <gayeongparkk@gmail.com>
2024-07-17 11:32:37 +0200

    conversion done

--- proof-parser/src/lib.rs
+++ proof-parser/src/lib.rs
@@ -5,6 +5,7 @@ use crate::{json_parser::ProofJSON, stark_proof::StarkProof};
 mod annotations;
 mod ast;
 mod builtins;
+mod conversion;
 mod json_parser;
 mod layout;
 mod stark_proof;
@@ -16,6 +17,7 @@ extern crate regex;
 extern crate serde;
 
 pub use ast::{Expr, Exprs};
+use cairovm_verifier_stark::types::StarkProof as StarkProofFromVerifier;
 
 pub struct ParseStarkProof {
     pub config: Exprs,
@@ -24,13 +26,25 @@ pub struct ParseStarkProof {
     pub witness: Exprs,
 }
 
-pub fn parse(input: String) -> anyhow::Result<ParseStarkProof> {
+pub fn parse(input: String) -> anyhow::Result<StarkProofFromVerifier> {
     let proof_json = serde_json::from_str::<ProofJSON>(&input)?;
     let stark_proof = StarkProof::try_from(proof_json)?;
-    Ok(ParseStarkProof {
-        config: Exprs::from(stark_proof.config),
-        public_input: Exprs::from(stark_proof.public_input),
-        unsent_commitment: Exprs::from(stark_proof.unsent_commitment),
-        witness: Exprs::from(stark_proof.witness),
-    })
+    let stark_proof_verifier: StarkProofFromVerifier = stark_proof.into();
+    Ok(stark_proof_verifier)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = include_str!(
+            "../../example/fibonacci_proof_generator/recursive/cairo0_example_proof.json"
+        );
+        let proof_json = serde_json::from_str::<ProofJSON>(input).unwrap();
+        let stark_proof = StarkProof::try_from(proof_json).unwrap();
+        let stark_proof_verifier: StarkProofFromVerifier = stark_proof.into();
+        println!("{:?}", stark_proof_verifier);
+    }
 }
