561a95d9074cbebf137e77e921d37c7f78af94d8
Bartosz Nowak <bartekn337@gmail.com>
2024-07-18 12:57:22 +0200

    decommitment unfriendly layers fix critical bug & blake2s & keccak verifier unfriendly hash function support

--- crates/pow/src/pow.rs
+++ crates/pow/src/pow.rs
@@ -1,8 +1,16 @@
+#[cfg(feature = "blake2s")]
+use blake2::Blake2s256;
+#[cfg(feature = "blake2s")]
+use blake2::Digest;
+#[cfg(feature = "keccak")]
+use sha3::Digest;
+#[cfg(feature = "keccak")]
+use sha3::Keccak256;
+
+use bail_out::assure;
 use cairovm_verifier_transcript::transcript::Transcript;
 use serde::{Deserialize, Serialize};
-use sha3::{Digest, Keccak256};
 use starknet_crypto::Felt;
-use starknet_types_core::felt::NonZeroFelt;
 
 use crate::config::Config;
 
@@ -27,7 +35,10 @@ pub fn verify_pow(digest: [u8; 32], n_bits: u8, nonce: u64) -> Result<(), Error>
     //      8 bytes            || 32 bytes || 1 byte
     // Total of 0x29 = 41 bytes.
 
+    #[cfg(feature = "keccak")]
     let mut hasher = Keccak256::new();
+    #[cfg(feature = "blake2s")]
+    let mut hasher = Blake2s256::new();
     let mut init_data = Vec::with_capacity(41);
     init_data.extend_from_slice(&MAGIC.to_be_bytes());
     init_data.extend_from_slice(&digest);
@@ -42,21 +53,20 @@ pub fn verify_pow(digest: [u8; 32], n_bits: u8, nonce: u64) -> Result<(), Error>
     //              32 bytes  || 8 bytes
     // Total of 0x28 = 40 bytes.
 
+    #[cfg(feature = "keccak")]
     let mut hasher = Keccak256::new();
+    #[cfg(feature = "blake2s")]
+    let mut hasher = Blake2s256::new();
     let mut hash_data = Vec::with_capacity(40);
     hash_data.extend_from_slice(&init_hash);
     hash_data.extend_from_slice(&nonce.to_be_bytes());
     hasher.update(&hash_data);
     let final_hash = hasher.finalize();
 
-    let work_limit = Felt::TWO.pow(128 - n_bits);
-    let (q, _r) = Felt::from_bytes_be_slice(final_hash.as_slice())
-        .div_rem(&NonZeroFelt::try_from(Felt::TWO.pow(128_u128)).unwrap());
-    if q >= work_limit {
-        Err(Error::ProofOfWorkFail)
-    } else {
-        Ok(())
-    }
+    assure!(
+        Felt::from_bytes_be_slice(&final_hash.as_slice()[0..16]) < Felt::TWO.pow(128 - n_bits),
+        Error::ProofOfWorkFail
+    )
 }
 
 use thiserror::Error;
