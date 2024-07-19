87b3a7d57c35054dcb34c6d7102b0240286a090f
Pia <gayeongparkk@gmail.com>
2024-07-19 20:58:40 +0200

    todo: need to dynamically select on wasm

--- wasm-binding/src/lib.rs
+++ wasm-binding/src/lib.rs
@@ -1,4 +1,4 @@
-use cairovm_verifier_air::layout::recursive;
+use cairovm_verifier_air::layout::starknet_with_keccak;
 use cairovm_verifier_proof_parser::parse;
 use wasm_bindgen::prelude::*;
 
@@ -13,7 +13,7 @@ pub fn wasm_verify(proof: JsValue) -> Result<JsValue, JsValue> {
     // Get security bits and verify
     let security_bits = stark_proof.config.security_bits();
     let (program_hash, output_hash) = stark_proof
-        .verify::<recursive::Layout>(security_bits)
+        .verify::<starknet_with_keccak::Layout>(security_bits)
         .map_err(|e| JsValue::from_str(&e.to_string()))?;
 
     // Serialize result to JsValue
