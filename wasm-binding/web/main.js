606ede19d721355c2a62ec770927340ecb56baa9
Pia <gayeongparkk@gmail.com>
2024-07-19 21:53:49 +0200

    simple wasm

--- wasm-binding/web/main.js
+++ wasm-binding/web/main.js
@@ -1,6 +1,5 @@
 import init, { wasm_verify } from '../pkg/wasm_binding.js';
 
-
 async function run() {
     await init();  // Initialize the Wasm module
 
@@ -17,8 +16,17 @@ async function run() {
         reader.onload = async function() {
             const proof = reader.result;
             try {
+                const startTime = Date.now(); // Start timing
                 const result = await wasm_verify(proof);
-                document.getElementById('output').innerText = JSON.stringify(result, null, 2);
+                const endTime = Date.now(); // End timing
+                const elapsedTime = endTime - startTime; // Calculate elapsed time
+
+                const parsedResult = JSON.parse(result);
+                
+                // Display program_hash and output_hash
+                document.getElementById('program-hash').innerText = `Program Hash: ${parsedResult[0]}`;
+                document.getElementById('output-hash').innerText = `Output Hash: ${parsedResult[1]}`;
+                document.getElementById('verification-time').innerText = `Verification Time: ${elapsedTime} ms`;
             } catch (err) {
                 console.error(err);
                 document.getElementById('output').innerText = `Verification failed: ${err}`;
